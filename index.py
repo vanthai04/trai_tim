50511543674f45ee38c31d6ad6c695d2783fe456
vanthai04 <thaitvph40872@fpt.edu.vn>
2025-12-12 17:17:43 +0700

    completed

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import turtle
 
-t = np.linspace(0, 2*np.pi, 100)
 
-x = 16 * np(t)**3
-y = 13*bnp.cos(t) - 5*np.cos(2*t)
+t = turtle.Turtle()
+t.speed(5)
+t.color("red")
+t.begin_fill()
+
+
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.left(120)
+t.circle(-90, 200)
+t.forward(180)
+
+t.end_fill()
+turtle.done()
